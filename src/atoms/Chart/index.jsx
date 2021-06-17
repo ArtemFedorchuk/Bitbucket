afb8f1b4307b2e1a8c55ff575d2c3a9634aa99b6
FedorchukArtem <artem.f.itlt@gmail.com>
2021-06-17 18:58:54 +0300

    update chart obj data & styles

--- src/atoms/Chart/index.jsx
+++ src/atoms/Chart/index.jsx
@@ -1,42 +1,81 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Chart from 'react-apexcharts';
 import { useSelector } from 'react-redux';
 import { DateFormater } from '../../helpers';
 import { MySpinner } from '../index';
 
 const WeatherChart = () => {
+  const [ data, setData ] = useState(null)
   const chartData = useSelector((state) => state.weather.chartData);
 
-  const data = {
-    options: {
-      title: {
-        text: chartData && `${chartData?.timezone} (temp F)`,
-        style: {
-          fontSize:  '14px',
-          fontWeight:  'bold',
-          color:  '#263238'
+  useEffect(() => {
+    const customData = {
+      options: {
+        title: {
+          text: chartData && `${chartData?.timezone} (temp F)`,
+          style: {
+            fontSize:  '14px',
+            fontWeight:  'bold',
+            color:  '#263238',
+          },
+          label: {
+            color: 'red'
+          }
+        },
+        chart: {
+          id: 'chart',
+        },
+        xaxis: {
+          categories: chartData &&
+            chartData?.hourly?.slice(0, 25).map(({dt}) => DateFormater(dt, "Asia/Kolkata"))
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 5,
+            dataLabels: {
+              position: 'center', // top, center, bottom
+            },
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: function (val) {
+            return Number(val).toFixed(0) + "F";
+          },
+          offsetY: -20,
+          style: {
+            fontSize: '12px',
+            colors: ["white"],
+          },
+          dropShadow: {
+            enabled: true,
+            top: 0,
+            left: 0,
+            blur: 1,
+            color: '#000',
+            opacity: 0.35
+          }
         },
       },
-      chart: {
-        id: 'chart',
-      },
-      xaxis: {
-        categories: chartData &&
-          chartData?.hourly?.slice(0, 25).map(({dt}) => DateFormater(dt, "Asia/Kolkata"))
-      }
-    },
-    series: [{
-      name: 'temp F',
-      data: chartData &&
-        chartData?.hourly?.slice(0, 25).map(({temp}) => temp)
-    }]
-  }
+      series: [{
+        name: 'temp F',
+        data: chartData &&
+          chartData?.hourly?.slice(0, 25).map(({temp}) => temp)
+      }],
+    }
+
+    const clear = setTimeout(() => {
+      setData(customData)
+    }, 400)
+
+    return () => clearTimeout(clear)
+  }, [chartData])
 
   return (
     <div>
-      {chartData ? (
+      {data ? (
         <Chart options={data?.options} series={data?.series} type="bar" width='100%' height={600} />
-      ): (<MySpinner/>)}
+      ): (<MySpinner />)}
     </div>
   );
 };
